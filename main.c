9093a3e7decadeeb0233a977c70095c70d63d6f0
jontehojo@live.se <jontehojo@live.se>
2026-05-04 16:30:39 +0200

    first commit

--- main.c
+++ main.c
@@ -5,9 +5,50 @@ int main()
 {
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
-
-   
+    
     // Your code goes here
+    int diceThrows[100];
+    int ones = 0, twos = 0, threes = 0, fours = 0, fives = 0, sixes = 0;
+    int sum = 0;
+    float average = 0.0;
+
+    for(int i = 0; i < 100; i++) {
+        int throw = rand() % 6 + 1;
+        switch(throw) {
+            case 1:
+                ones++;
+                break;
+            case 2:
+                twos++;
+                break;
+            case 3:
+                threes++;
+                break;
+            case 4:
+                fours++;
+                break;
+            case 5:
+                fives++;
+                break;
+            case 6:
+                sixes++;
+                break;
+        }
+        sum += throw;
+        diceThrows[i] = throw;
+    }
+
+    average = sum / 100.0;
+
+    printf("%d\n", ones);
+    printf("%d\n", twos);
+    printf("%d\n", threes);
+    printf("%d\n", fours);
+    printf("%d\n", fives);
+    printf("%d\n", sixes);
+
+    printf("%d\n", sum);
+    printf("%.1f\n", average);
 
     return 0;
 }
